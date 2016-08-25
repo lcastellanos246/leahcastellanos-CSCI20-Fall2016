9d81e4d4ba844e26cdd16a1acf7e583ab3dcc1b9
Leah Castellanos <lcastellanos246@gmail.com>
2016-08-25 04:18:24 +0000

    Lab 1 folder for ASCII art

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+//Created By Leah Castellanos
+//Date 8/24/2016
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	cout<<"   __  __"<<endl;
+	cout<<"  |  ||  |"<<endl;
+	cout<<"__|0 ||0 | __"<<endl;
+	cout<<"| |__||__|  |"<<endl;
+	cout<<"|           |"<<endl;
+	cout<<"| -_______- |"<<endl;
+	cout<<"|___________|"<<endl;
+	
+}
